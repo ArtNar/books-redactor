e60185c99f0346681c59682ee31f97b8adf42939
Artem Narozhniy <artemnarozhniy@gmail.com>
2020-09-02 23:09:02 +0200

    Updates

---
+++ precache-manifest.b206fbdef59b7f72630056a45b592c44.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeaa442c830a13af29d31be2c2a7b8f3",
+    "url": "/books-redactor/index.html"
+  },
+  {
+    "revision": "0dcd9ef6bbfd4dbf27a0",
+    "url": "/books-redactor/static/css/3.0b5af59f.chunk.css"
+  },
+  {
+    "revision": "18134166e2f6e5364228",
+    "url": "/books-redactor/static/css/4.6a429d3f.chunk.css"
+  },
+  {
+    "revision": "809e6a6250cd2b7d1929",
+    "url": "/books-redactor/static/css/5.922d2a29.chunk.css"
+  },
+  {
+    "revision": "73ce273915d344519e51",
+    "url": "/books-redactor/static/css/6.2b475783.chunk.css"
+  },
+  {
+    "revision": "3e783b10880d18ac5d87",
+    "url": "/books-redactor/static/css/main.96b076ba.chunk.css"
+  },
+  {
+    "revision": "11f09a93a4177eb8fb05",
+    "url": "/books-redactor/static/js/2.82f0f884.chunk.js"
+  },
+  {
+    "revision": "bd49b04565accd5b6f2624c86f8385ba",
+    "url": "/books-redactor/static/js/2.82f0f884.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dcd9ef6bbfd4dbf27a0",
+    "url": "/books-redactor/static/js/3.51ba7c18.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/books-redactor/static/js/3.51ba7c18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18134166e2f6e5364228",
+    "url": "/books-redactor/static/js/4.32cacf87.chunk.js"
+  },
+  {
+    "revision": "809e6a6250cd2b7d1929",
+    "url": "/books-redactor/static/js/5.184ea45b.chunk.js"
+  },
+  {
+    "revision": "73ce273915d344519e51",
+    "url": "/books-redactor/static/js/6.ea09c21d.chunk.js"
+  },
+  {
+    "revision": "3e783b10880d18ac5d87",
+    "url": "/books-redactor/static/js/main.7447eeb3.chunk.js"
+  },
+  {
+    "revision": "28b1fe6f805f499285f8",
+    "url": "/books-redactor/static/js/runtime-main.b650b00f.js"
+  }
+]);
