194e7b02fdb2ffb46b915497f6c5e2dcad1d7a22
Artem Narozhniy <artemnarozhniy@gmail.com>
2020-09-02 23:31:06 +0200

    refactor

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,12 +1,12 @@
 import React                    from 'react'
 import Router                   from 'router'
-import { BrowserRouter }        from 'react-router-dom'
+import { HashRouter }           from 'react-router-dom'
 import { routes }               from 'router/routes'
 
 const App = () => (
-    <BrowserRouter>
+    <HashRouter basename="/">
         <Router routes={routes} />
-    </BrowserRouter>
+    </HashRouter>
 )
 
 export default App
